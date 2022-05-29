da406803b127aaa8e46313a0b3f2389e97b7c1ff
Sevda Shukrieva <sevda.shukrieva@xdesign.com>
2022-05-29 19:31:43 +0100

    interacting with DOM elements and fetching user input ::curly_haired_woman:

--- dist/app.js
+++ dist/app.js
@@ -3,4 +3,15 @@ let templateEl = document.getElementById('project-input');
 const app = document.getElementById('app');
 let templateElContent = templateEl.content;
 app.appendChild(templateElContent);
+const formEl = document.querySelector('form');
+const titleInputEl = document.querySelector('#title');
+const descriptionInputEl = document.querySelector('#description');
+const peopleInputEl = document.querySelector('#people');
+formEl.id = 'user-input';
+const submitHandler = (event) => {
+    event.preventDefault();
+    console.log('User input: ', titleInputEl.value);
+};
+formEl.addEventListener('submit', submitHandler);
+console.log('test_people-input', peopleInputEl);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,31 @@
+//Template
 let templateEl = <HTMLTemplateElement>document.getElementById('project-input');
 const app = <HTMLDivElement>document.getElementById('app')!;
-
 let templateElContent = templateEl.content
 app.appendChild(templateElContent) 
+
+
+// Elements
+const formEl =  <HTMLFormElement>document.querySelector('form');
+const titleInputEl = <HTMLInputElement>document.querySelector('#title')
+const descriptionInputEl = <HTMLInputElement>document.querySelector('#description')
+const peopleInputEl = <HTMLInputElement>document.querySelector('#people')
+
+// Add css selectors to the elements dynamically
+formEl.id = 'user-input'
+
+
+// Handler helpers
+const submitHandler = (event: Event) => {
+// prevent HTTP req send
+event.preventDefault();
+console.log('User input: ', titleInputEl.value)
+}
+
+
+// Event Handlers
+formEl.addEventListener('submit', submitHandler)
+
+
+console.log('test_people-input', peopleInputEl)
+
